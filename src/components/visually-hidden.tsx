c87dc4120d100514c46f89e261bada36ba947a9f
Jeremy Neander <jeremy.neander@gmail.com>
2023-02-17 08:51:42 -0600

    refactor: use typescript extensions

--- src/components/visually-hidden.tsx
+++ src/components/visually-hidden.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import {elementType} from 'prop-types'
 import React from 'react'
 
--- src/hooks/use-focus-scope.ts
+++ src/hooks/use-focus-scope.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import {useEffect} from 'react'
 
 import {findTabbable} from '@jneander/a11y-dom'
